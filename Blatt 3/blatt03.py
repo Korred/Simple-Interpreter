32c27e7293e3b7f913c3c2788d746f5fe89ab770
Philipp Kochanski <P.Kochanski@uni-duesseldorf.de>
2016-04-27 19:08:26 +0200

    Added solution to 3rd exercise
    Finished: 2
    Missing: 1 / 3

---
+++ Blatt 3/blatt03.py
@@ -0,0 +1,46 @@
+# AUFGABE 2
+
+def lifestep(field):
+	offset = range(-1,2)
+	new_field=set()
+	# for every neighbour of a living cell
+	# count adjacent living cells
+	# if neighbour was a living cell and has 2-3 adjacent living cells -> add to new field
+	# if neighbour was a dead cell and has 3 adjacent living cells -> add to new field
+
+	for cell in field:
+		for y in offset:
+			for x in offset:
+				check=(cell[0]+x,cell[1]+y)
+				adjacent = 0
+				for cy in offset:
+					for cx in offset:
+						neighbour = (check[0]+cy, check[1]+cx)
+						if neighbour in field and neighbour != check:
+							adjacent += 1
+				if check in field:
+					if adjacent == 2 or adjacent == 3:
+						new_field.add(check)
+				else:
+					if adjacent == 3:
+						new_field.add(check)
+						
+	return new_field
+
+def lifestring(field):
+	min_val = list(map(min, zip(*field)))  # holds [x-min, y-min] values of field
+	max_val = list(map(max, zip(*field)))  # holds [x-max, y-max] values of field
+
+	string = []
+
+	if field:
+		for y in range(min_val[1], max_val[1]+1):
+			for x in range(min_val[0], max_val[0]+1):
+				if (x,y) in field:
+					string.append("X")
+				else:
+					string.append(" ")
+			if y != max_val[1]:  # just add newline "\n" when line is not last line
+				string.append("\n")
+
+	return "".join(string)
