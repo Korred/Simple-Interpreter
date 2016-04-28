46d8d61be0c141e5178a3ffd3eea1a5cfbc2c772
Philipp Kochanski <P.Kochanski@uni-duesseldorf.de>
2016-04-28 08:59:25 +0200

    Restructuring

--- Blatt 3/blatt03.py
+++ Blatt 3/blatt03.py
@@ -1,60 +1,66 @@
+# AUFGABE 1
+
+# mygetattribute nowhere to be found!
+
 # AUFGABE 2
 
-def lifestep(field):
-	offset = range(-1,2)
-	new_field=set()
-	# for every neighbour of a living cell
-	# count adjacent living cells
-	# if neighbour was a living cell and has 2-3 adjacent living cells -> add to new field
-	# if neighbour was a dead cell and has 3 adjacent living cells -> add to new field
 
-	for cell in field:
-		for y in offset:
-			for x in offset:
-				check=(cell[0]+x,cell[1]+y)
-				adjacent = 0
-				for cy in offset:
-					for cx in offset:
-						neighbour = (check[0]+cy, check[1]+cx)
-						if neighbour in field and neighbour != check:
-							adjacent += 1
-				if check in field:
-					if adjacent == 2 or adjacent == 3:
-						new_field.add(check)
-				else:
-					if adjacent == 3:
-						new_field.add(check)
-						
-	return new_field
+def lifestep(field):
+    offset = range(-1, 2)
+    new_field = set()
+    # for every neighbour of a living cell
+    # count adjacent living cells
+    # if neighbour was a living cell and has 2-3 adjacent living cells -> add to new field
+    # if neighbour was a dead cell and has 3 adjacent living cells -> add to new field
+
+    for cell in field:
+        for y in offset:
+            for x in offset:
+                check = (cell[0] + x, cell[1] + y)
+                adjacent = 0
+                for cy in offset:
+                    for cx in offset:
+                        neighbour = (check[0] + cy, check[1] + cx)
+                        if neighbour in field and neighbour != check:
+                            adjacent += 1
+                if check in field:
+                    if adjacent == 2 or adjacent == 3:
+                        new_field.add(check)
+                else:
+                    if adjacent == 3:
+                        new_field.add(check)
+
+    return new_field
+
 
 def lifestring(field):
-	min_val = list(map(min, zip(*field)))  # holds [x-min, y-min] values of field
-	max_val = list(map(max, zip(*field)))  # holds [x-max, y-max] values of field
+    min_val = list(map(min, zip(*field)))  # holds [x-min, y-min] values of field
+    max_val = list(map(max, zip(*field)))  # holds [x-max, y-max] values of field
 
-	string = []
+    string = []
 
-	if field:
-		for y in range(min_val[1], max_val[1]+1):
-			for x in range(min_val[0], max_val[0]+1):
-				if (x,y) in field:
-					string.append("X")
-				else:
-					string.append(" ")
-			if y != max_val[1]:  # just add newline "\n" when line is not last line
-				string.append("\n")
+    if field:
+        for y in range(min_val[1], max_val[1] + 1):
+            for x in range(min_val[0], max_val[0] + 1):
+                if (x, y) in field:
+                    string.append("X")
+                else:
+                    string.append(" ")
+            if y != max_val[1]:  # just add newline "\n" when line is not last line
+                string.append("\n")
 
-	return "".join(string)
+    return "".join(string)
 
 
 # AUFGABE 3
 
 class OpenClass(type):
-	def __new__(cls,name,bases,attr):
-		if name == "__enhance__":
-			att_keys = [key for key in attr if not key.startswith("__")]
-			for b in bases:
-				for key in att_keys:
-					setattr(b,key,attr[key])
-		else:
-			# return type.__new__(cls, name, bases, attr)
-			return super(OpenClass,cls).__new__(cls, name, bases, attr)
+    def __new__(cls, name, bases, attr):
+        if name == "__enhance__":
+            att_keys = [key for key in attr if not key.startswith("__")]
+            for b in bases:
+                for key in att_keys:
+                    setattr(b, key, attr[key])
+        else:
+            # return type.__new__(cls, name, bases, attr)
+            return super(OpenClass, cls).__new__(cls, name, bases, attr)
