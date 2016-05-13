963c6f1fa5a69f03daee6ffd2b163f019a8744b7
schmidt <joshua.schmidt@uni-duesseldorf.de>
2016-05-13 11:36:16 +0200

    complete task 3

--- Blatt 5/blatt05.py
+++ Blatt 5/blatt05.py
@@ -3,6 +3,19 @@
 # Aufgabe 2
 
 # Aufgabe 3
+def mybin(integer):
+    if integer == 0:
+        return "0"
+    else:
+        bin = []
+        while integer > 0:
+            if (round(integer) & 1) == 0:
+                bin = ["0"] + bin      # append to front, because we need to read bits backwards
+            else:
+                bin = ["1"] + bin
+            integer = integer // 2
+        bin = ["0b"] + bin
+        return "".join(bin)
 
 def myint(bin_string):
     cut_string = bin_string[2:][::-1]
