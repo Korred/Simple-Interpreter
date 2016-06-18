8a650115a6fb491ec01e4985cf3b7ad7fa229c6b
Philipp Kochanski <P.Kochanski@uni-duesseldorf.de>
2016-06-18 19:16:32 +0200

    Fixing merge issue

--- Blatt 9/objmodel.py
+++ Blatt 9/objmodel.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # prototype-based object model of the language
 '''
 There are three types of objects: integers, "normal" objects and methods.
@@ -12,8 +11,6 @@ Instead they should have an interface that the interpreter can use.
 here are some tests about this in test_objmodel.py.
 Write more.
 '''
-import copy
-
 
 class W_NormalObject(object):
     # Prototype
@@ -27,28 +24,11 @@ class W_NormalObject(object):
         try:
             return self.attrs[name]
         except (KeyError, AttributeError):
-=======
-
-class W_NormalObject(object):
-
-    def __init__(self,attrs={}):
-        self.attrs = attrs
-
-    def setvalue(self,name,value):
-        self.attrs[name] = value
-
-    def getvalue(self,name):
-        if (name in self.attrs):
-            return self.attrs[name]
-        else:
->>>>>>> origin/master
-            return None
 
     def istrue(self):
         return True
 
     def clone(self):
-<<<<<<< HEAD
         # dict(self.attrs) needed to create a copy of self.attrs
         return W_NormalObject(dict(self.attrs))
 
@@ -59,24 +39,9 @@ class W_Integer(W_NormalObject):
 
     def setvalue(obj, name, value):
         pass  # or raise NotImplementedError()
-=======
-        return W_NormalObject(dict(self.attrs))
-
-class W_Integer(W_NormalObject):
-
-    def __init__(self,value):
-        self.value = value
-
-    def getvalue(self,name):
-        return None
->>>>>>> origin/master
 
     def istrue(self):
         return self.value != 0
 
 class W_Method(W_NormalObject):
-<<<<<<< HEAD
     pass
-=======
-    pass
->>>>>>> origin/master
